5bc3f4863503b94f598cadeed31a961561d75f4a
Mang <mangemmanuel07@gmail.com>
2023-03-18 16:39:37 +0800

    Completed assignment

---
+++ index.js
@@ -0,0 +1,20 @@
+// has a function expression called divide
+// divide divides 2000 by 100
+
+function divide() {
+    return 2000 / 100;
+   };
+
+// has an arrow function called square
+// square arrow function takes one parameter and multiplies it times itself
+
+let x = 2;
+const square = x => x * 2;
+
+// has an arrow function called add
+// add arrow function takes two parameters and adds them together
+
+let a = 3;
+let b = 4;
+const add = (a, b) => a + b;
+add(3,4); 
